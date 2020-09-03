4d5203ea247369ee1fc149cef559d45883016949
devHectorGa <contacto@devHector.Ga>
2020-09-03 07:20:21 -0500

    Clase 01/09/20 - Primeros pasos con R y R Studio Server

---
+++ 01.r
@@ -0,0 +1,13 @@
+# calculo de n_5
+0.0603*7614
+# 459.1242
+
+# calculo de n_7
+7614 - 145 - 2415 - 3456 - 852 - 459 - 157
+# 130
+
+# Frecuencias absolutas
+nj <- c(145, 2415, 3456, 852, 459, 157, 130)
+
+# Tamaño de la muestra
+n <- 7614
