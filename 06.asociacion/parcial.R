1cd472cc1b39daa8a9b81d40ec128d3d168af0a5
devHectorGa <contacto@devHector.Ga>
2020-10-06 07:51:58 -0500

    Parcial 1 completo

--- 06.asociacion/parcial.R
+++ 06.asociacion/parcial.R
@@ -1,5 +1,28 @@
-tabla <- cbind(c(223, 53), c(91,11))
-n <- 378
+library(rstudioapi)
+filename = "parcial.r"
+filepath = rstudioapi::getActiveDocumentContext()$path
+dir = substr(filepath, 1, nchar(filepath)-nchar(filename))
+setwd(dir)
+rm(filename, filepath,dir)
+
+# Punto 1
+datos <- read.table(file = "datos_brutos.csv", header = TRUE, sep = ";")
+summary(datos$x)
+cv <- sd(datos$x)/mean(datos$x)
+d <- c(datos$x)
+q <- quantile(d)
+q[4]-q[2]
+summary(d)
+cv <- sd(d)/mean(d)
+Hj <- cumsum(table(datos$x)/50)
+cv
+quantile(d, probs = 0.8)
+R <- max(datos$x) - min(datos$x)
+
+
+# Punto 3
+tabla <- cbind(c(230, 57), c(91,13))
+n <- 391
 
 colnames(tabla) <- c("Cigarrillos Si", "Cigarrillo No")
 rownames(tabla) <- c("Alcohol Sí", "Alcohol No")
@@ -14,11 +37,9 @@ round(addmargins(A = pf, margin = 2), digits = 2)
 # PC
 round(addmargins(A = pc, margin = 1), digits = 2)
 
+# Punto 5
 
-
-### OTRO PUNTO
-
-nj <- c(1,1,1,2,16,43,76,72,41,1)
+nj <- c(1,1,1,3,15,46,63,83,40,1)
 n <- sum(nj)
 hj <- nj / n
 
@@ -27,7 +48,7 @@ Hj <- cumsum(hj)
 
 #Marca de clase
 li <- c(0,0.5,1,1.5,2,2.5,3,3.5,4,4.5)
-ls <- c(0.5,1,1.5,2,2.5,3,3.5,4,4.5,5 )
+ls <- c(0.5,1,1.5,2,2.5,3,3.5,4,4.5,5)
 x <- (li+ls)/2
 
 xbar = sum(x*nj)/n
@@ -37,3 +58,8 @@ sum(hj[9:10])*100
 xbar
 
 xbar * n - 1 * n
+
+s2 <- sum(nj*((x-xbar)^2))/n
+s = sqrt(s2)
+cv <- 100*s/xbar
+cv
